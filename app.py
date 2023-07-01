098f91c80a5bd84a0f47aaedb1a37cea8cb1b82c
rahul <rahularumugam04@gmail.com>
2023-07-01 16:52:18 +0530

    first commit

--- app.py
+++ app.py
@@ -10,12 +10,24 @@ model = pickle.load(open("model.pkl", "rb"))
 def Home():
     return render_template("index.html")
 
+@flask_app.route("/contact")
+def contact():
+    return render_template("contact.html")
+
+@flask_app.route("/pred",methods =["GET"])
+def Predict():
+    return render_template("pre.html")
+
+
 @flask_app.route("/predict", methods = ["POST"])
 def predict():
     float_features = [float(x) for x in request.form.values()]
     features = [np.array(float_features)]
     prediction = model.predict(features)
-    return render_template("index.html", prediction_text = "The chance to get admission for Higher Studies {}".format(prediction))
+    prediction2= prediction * 100.0
+    # return jsonify({'predictions':  prediction2.tolist()})
+    # return render_template("pre.html", prediction_text = "The chance to get admission for Higher Studies {} %".format(prediction))
+    return render_template("pre.html", prediction_text = format(prediction2))
 
 if __name__ == "__main__":
     flask_app.run(debug=True)
